8a5cadadc36256e0f5c77ed5f447de3b1e4cc49b
AhmadElMousa <ahmadelmousa@outlook.com>
2025-03-17 03:38:29 +0100

    implemented netlify serverless function

---
+++ serverless/jokes.js
@@ -0,0 +1,19 @@
+exports.handler = async (event, context) => {
+  const url = "https://icanhazdadjoke.com";
+  try {
+    const jokeStream = await fetch(url, {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+
+    const jsonJoke = await jokeStream.json();
+    const joke = jsonJoke.joke;
+    return {
+      statusCode: 200,
+      body: JSON.stringify(jsonJoke),
+    };
+  } catch (error) {
+    return { statusCode: 422, body: error.stack };
+  }
+};
